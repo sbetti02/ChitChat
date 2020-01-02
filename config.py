53581214eb1a7abab7ce9929e05278b481f9582c
Scott Bettigole <scott.bettigole@tufts.edu>
2020-01-02 00:20:14 -0500

    Added error logging/handling to both files and email

--- config.py
+++ config.py
@@ -9,3 +9,10 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'ScottMessager.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['scott.bettigole@gmail.com']
+    SEND_EMAILS = False
